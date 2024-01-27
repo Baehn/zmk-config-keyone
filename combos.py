9091cce5a65d94cd045c8a9b3d1f6e5ce4f60157
chbaeh <chbaeh@gmail.com>
2024-01-27 11:16:48 +0100

    up

---
+++ combos.py
@@ -0,0 +1,13 @@
+text = """
+        combo_NUM1 {
+            timeout-ms = <50>;
+            key-positions = <NUM1>;
+            bindings = <&kp KEY>;
+            layers = <1>
+        };
+"""
+
+for i, x in enumerate(range(14, 32)):
+    out = text.replace("NUM1", f"{x}")
+    out = out.replace("KEY", chr(i+65))
+    print(out)
